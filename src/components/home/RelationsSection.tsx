c77ec68e6c554039e1807245fe9903cf5216b4ff
pnk-ltoc-892 <rkpankajsingh151@gmail.com>
2025-07-29 23:14:57 +0530

    Fix Image Tag Error

--- src/components/home/RelationsSection.tsx
+++ src/components/home/RelationsSection.tsx
@@ -8,31 +8,43 @@ export default function RelationsSection() {
             <Image
                 src="https://motion-primitives.com/apple_music_logo.svg"
                 alt="Apple Music logo"
+                height={120}
+                width={0}
                 className="h-[120px] w-auto"
             />
             <Image
                 src="https://motion-primitives.com/chrome_logo.svg"
                 alt="Chrome logo"
+                height={120}
+                width={0}
                 className="h-[120px] w-auto"
             />
             <Image
                 src="https://motion-primitives.com/strava_logo.svg"
                 alt="Strava logo"
+                height={120}
+                width={0}
                 className="h-[120px] w-auto"
             />
             <Image
                 src="https://motion-primitives.com/nintendo_logo.svg"
                 alt="Nintendo logo"
+                height={120}
+                width={0}
                 className="h-[120px] w-auto"
             />
             <Image
                 src="https://motion-primitives.com/jquery_logo.svg"
                 alt="Jquery logo"
+                height={120}
+                width={0}
                 className="h-[120px] w-auto"
             />
             <Image
                 src="https://motion-primitives.com/prada_logo.svg"
                 alt="Prada logo"
+                height={120}
+                width={0}
                 className="h-[120px] w-auto"
             />
         </InfiniteSlider>
